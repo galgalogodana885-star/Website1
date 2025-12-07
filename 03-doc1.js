a7a1fa08fe6f5e2dfacdc13a62a5654bfff34662
Galgalo Godana Dureti <galgalogodana885@gmail.com>
2025-12-07 12:54:38 +0300

    Add files via upload

---
+++ 03-doc1.js
@@ -0,0 +1,4 @@
+document.querySelector(".js-button1").onclick = function() {
+    let userInput = document.getElementById("input1").value;
+    alert("Hello, " + userInput + "! Welcome to my website.");
+}   
